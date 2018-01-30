533028e473776b84b26f392297620d738aa36d13
37i3i3 <945580817@qq.com>
2018-01-31 01:50:41 +0800

    Add files via upload

---
+++ pages/page10000/page10000.js
@@ -0,0 +1,99 @@
+var app      = getApp();
+
+var pageData = {
+  data: {"static_vessel1":{"type":"static-vessel",
+      "style":"background-color:rgba(0, 0, 0, 0);margin-left:0;margin-top:0;opacity:1;",
+      "content":[{"type":"free-vessel",
+          "style":"width:750rpx;height:389.0625rpx;margin-bottom:auto;margin-right:auto;opacity:1;margin-left:auto;",
+          "content":[{"type":"picture",
+              "style":"width:679.6875rpx;margin-top:0;height:267.1875rpx;margin-bottom:9.375rpx;opacity:1;left:35.15625rpx;margin-left:0;margin-right:0;position:absolute;right:auto;",
+              "content":"http:\/\/img.weiye.me\/zcimgdir\/album\/file_581c41bf3fc04.gif",
+              "customFeature":{"action":"custom-link",
+                  "page-link":"http:\/\/www.dfsdongwha.com\/cn\/event_mall\/event_mall_detail.asp?seq=2794",
+                  "custom-page-link":"http:\/\/www.dfsdongwha.com\/cn\/event_mall\/event_mall_detail.asp?seq=2794"},
+              "animations":[],
+              "compId":"data.content[0]","parentCompid":"data.content[0]"},
+              {"type":"text",
+                  "style":"background-color:rgba(0, 0, 0, 0);border-color:rgb(34, 34, 34);border-style:none;border-width:4.6875rpx;color:rgb(102, 102, 102);font-size:32.8125rpx;height:44.53125rpx;line-height:44.53125rpx;margin-left:0;margin-top:0;opacity:1;text-align:left;position:absolute;left:260.15625rpx;top:328.125rpx;margin-right:0;right:auto;",
+                  "content":"\u9884\u5b58\u4f59\u989d\uff08\u5143\uff09",
+                  "customFeature":{"boxColor":"rgb(0, 0, 0)",
+                      "boxR":"5",
+                      "boxStyle":false,
+                      "boxX":"0",
+                      "boxY":"0"
+                  },
+                  "animations":[],
+                  "compId":"data.content[1]",
+                  "parentCompid":"data.content[0]",
+                  "markColor":"",
+                  "mode":0
+              },
+              {"type":"text",
+                  "style":"background-color:rgba(0, 0, 0, 0);border-color:rgb(34, 34, 34);border-style:none;border-width:4.6875rpx;color:rgb(175, 24, 74);font-size:32.8125rpx;height:44.53125rpx;line-height:44.53125rpx;margin-left:0;margin-top:0;opacity:1;text-align:left;position:absolute;left:365.625rpx;top:283.59375rpx;margin-right:0;right:auto;",
+                  "content":"0",
+                  "customFeature":{"boxColor":"rgb(0, 0, 0)",
+                      "boxR":"5",
+                      "boxStyle":false,
+                      "boxX":"0",
+                      "boxY":"0"
+                  },
+                  "animations":[],
+                  "compId":"data.content[2]",
+                  "parentCompid":"data.content[0]",
+                  "markColor":"",
+                  "mode":0
+              }],
+          "customFeature":{"boxColor":"rgb(0, 0, 0)",
+              "boxR":5,
+              "boxStyle":false,
+              "boxX":0,
+              "boxY":0},
+          "animations":[],
+          "parentCompid":"static_vessel1",
+          "compId":"data.content[0]"}],
+      "customFeature":{"boxColor":"rgb(0, 0, 0)",
+          "boxR":"5px","boxStyle":false,"boxX":"0","boxY":"0"},
+      "animations":[],"page_form":"","compId":"static_vessel1"},"free_vessel2":{"type":"free-vessel","style":"margin-top:35.15625rpx;height:105.46875rpx;background-color:rgb(255, 255, 255);opacity:1;margin-left:auto;","content":[{"type":"text","style":"position:absolute;margin:0;left:117.1875rpx;top:30.46875rpx;right:auto;margin-left:auto;","content":"\u9884\u5b58\u8bb0\u5f55","customFeature":[],"animations":[],"compId":"data.content[0]","parentCompid":"free_vessel2"},{"type":"picture","style":"position:absolute;margin:0;left:703.125rpx;top:35.15625rpx;background-color:rgb(255, 255, 255);width:23.4375rpx;height:35.15625rpx;opacity:1;margin-left:auto;","content":"http:\/\/img.weiye.me\/zcimgdir\/album\/file_5a702f0a5a29f.png","customFeature":[],"animations":[],"compId":"data.content[1]","parentCompid":"free_vessel2"},{"type":"picture","style":"position:absolute;margin:0;left:30.46875rpx;top:25.78125rpx;width:46.875rpx;height:49.21875rpx;opacity:1;margin-left:auto;","content":"http:\/\/img.weiye.me\/zcimgdir\/album\/file_56cfec0991900.png","customFeature":[],"animations":[],"compId":"data.content[2]","parentCompid":"free_vessel2"}],"customFeature":{"action":"inner-link","page-link":"page10003","inner-page-link":"page10003"},"animations":[],"page_form":"","compId":"free_vessel2","itemType":"free-vessel","itemParentType":null,"itemIndex":"free_vessel2","eventParams":"{\"inner_page_link\":\"\\\/pages\\\/page10003\\\/page10003\",\"is_redirect\":0}","eventHandler":"tapInnerLinkHandler"},"free_vessel3":{"type":"free-vessel","style":"height:105.46875rpx;background-color:rgb(255, 255, 255);opacity:1;margin-left:auto;","content":[{"type":"text","style":"position:absolute;margin:0;left:117.1875rpx;top:30.46875rpx;right:auto;margin-left:auto;","content":"\u53ef\u7528\u79ef\u5206","customFeature":[],"animations":[],"compId":"data.content[0]","parentCompid":"free_vessel3"},{"type":"picture","style":"position:absolute;margin:0;left:703.125rpx;top:35.15625rpx;background-color:rgb(255, 255, 255);width:23.4375rpx;height:35.15625rpx;opacity:1;margin-left:auto;","content":"http:\/\/img.weiye.me\/zcimgdir\/album\/file_5a702f0a5a29f.png","customFeature":[],"animations":[],"compId":"data.content[1]","parentCompid":"free_vessel3"},{"type":"picture","style":"position:absolute;margin:0;left:30.46875rpx;top:25.78125rpx;width:46.875rpx;height:49.21875rpx;opacity:1;margin-left:auto;","content":"http:\/\/img.weiye.me\/zcimgdir\/album\/file_56cfec0bd635d.png","customFeature":[],"animations":[],"compId":"data.content[2]","parentCompid":"free_vessel3"}],"customFeature":{"action":"inner-link","page-link":"page10003","inner-page-link":"page10003"},"animations":[],"page_form":"","compId":"free_vessel3","itemType":"free-vessel","itemParentType":null,"itemIndex":"free_vessel3","eventParams":"{\"inner_page_link\":\"\\\/pages\\\/page10003\\\/page10003\",\"is_redirect\":0}","eventHandler":"tapInnerLinkHandler"},"free_vessel4":{"type":"free-vessel","style":"height:105.46875rpx;background-color:rgb(255, 255, 255);opacity:1;margin-left:auto;","content":[{"type":"text","style":"position:absolute;margin:0;left:117.1875rpx;top:30.46875rpx;right:auto;margin-left:auto;","content":"\u4f18\u60e0\u5238","customFeature":[],"animations":[],"compId":"data.content[0]","parentCompid":"free_vessel4"},{"type":"picture","style":"position:absolute;margin:0;left:703.125rpx;top:35.15625rpx;background-color:rgb(255, 255, 255);width:23.4375rpx;height:35.15625rpx;opacity:1;margin-left:auto;","content":"http:\/\/img.weiye.me\/zcimgdir\/album\/file_5a702f0a5a29f.png","customFeature":[],"animations":[],"compId":"data.content[1]","parentCompid":"free_vessel4"},{"type":"picture","style":"position:absolute;margin:0;left:30.46875rpx;top:25.78125rpx;width:46.875rpx;height:49.21875rpx;opacity:1;margin-left:auto;","content":"http:\/\/img.weiye.me\/zcimgdir\/album\/file_56cfec0c5bcfa.png","customFeature":[],"animations":[],"compId":"data.content[2]","parentCompid":"free_vessel4"}],"customFeature":{"action":"inner-link","page-link":"page10003","inner-page-link":"page10003"},"animations":[],"page_form":"","compId":"free_vessel4","itemType":"free-vessel","itemParentType":null,"itemIndex":"free_vessel4","eventParams":"{\"inner_page_link\":\"\\\/pages\\\/page10003\\\/page10003\",\"is_redirect\":0}","eventHandler":"tapInnerLinkHandler"},"free_vessel5":{"type":"free-vessel","style":"margin-top:35.15625rpx;height:105.46875rpx;background-color:rgb(255, 255, 255);opacity:1;margin-left:auto;","content":[{"type":"text","style":"position:absolute;margin:0;left:117.1875rpx;top:30.46875rpx;right:auto;margin-left:auto;","content":"\u6d88\u8d39\u8d26\u5355","customFeature":[],"animations":[],"compId":"data.content[0]","parentCompid":"free_vessel5"},{"type":"picture","style":"position:absolute;margin:0;left:703.125rpx;top:35.15625rpx;background-color:rgb(255, 255, 255);width:23.4375rpx;height:35.15625rpx;opacity:1;margin-left:auto;","content":"http:\/\/img.weiye.me\/zcimgdir\/album\/file_5a702f0a5a29f.png","customFeature":[],"animations":[],"compId":"data.content[1]","parentCompid":"free_vessel5"},{"type":"picture","style":"position:absolute;margin:0;left:30.46875rpx;top:25.78125rpx;width:46.875rpx;height:49.21875rpx;opacity:1;margin-left:auto;","content":"http:\/\/img.weiye.me\/zcimgdir\/album\/file_56cfec09eb1fe.png","customFeature":[],"animations":[],"compId":"data.content[2]","parentCompid":"free_vessel5"}],"customFeature":{"action":"inner-link","page-link":"page10003","inner-page-link":"page10003"},"animations":[],"page_form":"","compId":"free_vessel5","itemType":"free-vessel","itemParentType":null,"itemIndex":"free_vessel5","eventParams":"{\"inner_page_link\":\"\\\/pages\\\/page10003\\\/page10003\",\"is_redirect\":0}","eventHandler":"tapInnerLinkHandler"},"free_vessel6":{"type":"free-vessel","style":"margin-top:35.15625rpx;height:105.46875rpx;background-color:rgb(255, 255, 255);opacity:1;margin-left:auto;","content":[{"type":"text","style":"position:absolute;margin:0;left:117.1875rpx;top:30.46875rpx;right:auto;margin-left:auto;","content":"\u4fee\u6539\u5bc6\u7801","customFeature":[],"animations":[],"compId":"data.content[0]","parentCompid":"free_vessel6"},{"type":"picture","style":"position:absolute;margin:0;left:703.125rpx;top:35.15625rpx;background-color:rgb(255, 255, 255);width:23.4375rpx;height:35.15625rpx;opacity:1;margin-left:auto;","content":"http:\/\/img.weiye.me\/zcimgdir\/album\/file_5a702f0a5a29f.png","customFeature":[],"animations":[],"compId":"data.content[1]","parentCompid":"free_vessel6"},{"type":"picture","style":"position:absolute;margin:0;left:30.46875rpx;top:25.78125rpx;width:46.875rpx;height:49.21875rpx;opacity:1;margin-left:auto;","content":"http:\/\/img.weiye.me\/zcimgdir\/album\/file_56cfec0b60efb.png","customFeature":[],"animations":[],"compId":"data.content[2]","parentCompid":"free_vessel6"}],"customFeature":{"action":"inner-link","page-link":"page10003","inner-page-link":"page10003"},"animations":[],"page_form":"","compId":"free_vessel6","itemType":"free-vessel","itemParentType":null,"itemIndex":"free_vessel6","eventParams":"{\"inner_page_link\":\"\\\/pages\\\/page10003\\\/page10003\",\"is_redirect\":0}","eventHandler":"tapInnerLinkHandler"},"has_tabbar":0,"page_hidden":true,"page_form":"","top_nav":{"navigationBarBackgroundColor":"#AF184A","navigationBarTextStyle":"black","navigationBarTitleText":"\u6211\u7684\u5361"}},
+    need_login: false,
+    page_router: 'page10000',
+    page_form: 'none',
+      list_compids_params: [],
+      user_center_compids_params: [],
+      goods_compids_params: [],
+  prevPage:0,
+      tostoreComps: [],
+      carouselGroupidsParams: [],
+      relobj_auto: [],
+      bbsCompIds: [],
+      dynamicVesselComps: [],
+      communityComps: [],
+      franchiseeComps: [],
+      cityLocationComps: [],
+      seckillOnLoadCompidParam: [],
+      dynamicClassifyGroupidsParams: [],
+      videoListComps: [],
+      videoProjectComps: [],
+      returnToVersionFlag: true,
+  requesting: false,
+  requestNum: 1,
+  modelChoose: [],
+  modelChooseId: '',
+  modelChooseName: [],
+  onLoad: function (e) {
+    app.onPageLoad(e);
+  },
+  dataInitial: function () {
+    app.pageDataInitial();
+  },
+  onShareAppMessage: function (e) {
+    return app.onPageShareAppMessage(e);
+  },
+  onShow: function () {
+    app.onPageShow();
+  },
+  reachBottomFuc: [],
+  onReachBottom: function () {
+    app.onPageReachBottom( this.reachBottomFuc );
+  }
+};
+Page(pageData);
---
+++ pages/page10001/page10001.js
@@ -0,0 +1,391 @@
+var app      = getApp();
+
+var pageData = {
+  data: {"free_vessel1":{"type":"free-vessel","style":"margin-top:35.15625rpx;background-color:rgb(255, 255, 255);height:93.75rpx;opacity:1;margin-left:auto;","content":[{"type":"text","style":"position:absolute;margin:0;left:79.6875rpx;top:23.4375rpx;color:rgb(102, 102, 102);font-size:30.46875rpx;text-align:center;margin-left:auto;","content":"\u65f6\u95f4","customFeature":[],"animations":[],"compId":"data.content[0]","parentCompid":"free_vessel1"},{"type":"text","style":"position:absolute;margin:0;left:267.1875rpx;top:23.4375rpx;color:rgb(102, 102, 102);font-size:30.46875rpx;width:215.625rpx;height:56.25rpx;text-align:center;right:auto;margin-left:auto;","content":"\u9884\u5b58\u91d1\u989d\uff08\u5143\uff09","customFeature":[],"animations":[],"compId":"data.content[1]","parentCompid":"free_vessel1"},{"type":"text","style":"position:absolute;margin:0;left:560.36496162415rpx;top:23.4375rpx;color:rgb(102, 102, 102);font-size:30.46875rpx;margin-left:auto;","content":"\u5145\u503c\u8d60\u5238","customFeature":[],"animations":[],"compId":"data.content[2]","parentCompid":"free_vessel1"}],"customFeature":[],"animations":[],"page_form":"","compId":"free_vessel1"},"text2":{"type":"text","style":"background-color:rgba(0, 0, 0, 0);border-color:rgb(34, 34, 34);border-style:none;border-width:4.6875rpx;color:rgb(102, 102, 102);font-size:32.8125rpx;height:44.53125rpx;line-height:44.53125rpx;margin-left:0;margin-top:28.125rpx;opacity:1;text-align:center;","content":"\u6682\u65e0\u6570\u636e","customFeature":{"boxColor":"rgb(0, 0, 0)","boxR":"5","boxStyle":false,"boxX":"0","boxY":"0"},"animations":[],"page_form":"","compId":"text2","parentCompid":"text2","markColor":"","mode":0},"has_tabbar":0,"page_hidden":true,"page_form":"","top_nav":{"navigationBarBackgroundColor":"#ffffff","navigationBarTextStyle":"white","navigationBarTitleText":"\u9884\u5b58\u8bb0\u5f55"}},
+    need_login: false,
+    page_router: 'page10001',
+    page_form: 'none',
+      list_compids_params: [],
+      user_center_compids_params: [],
+      goods_compids_params: [],
+  prevPage:0,
+      tostoreComps: [],
+      carouselGroupidsParams: [],
+      relobj_auto: [],
+      bbsCompIds: [],
+      dynamicVesselComps: [],
+      communityComps: [],
+      franchiseeComps: [],
+      cityLocationComps: [],
+      seckillOnLoadCompidParam: [],
+      dynamicClassifyGroupidsParams: [],
+      videoListComps: [],
+      videoProjectComps: [],
+      returnToVersionFlag: true,
+  requesting: false,
+  requestNum: 1,
+  modelChoose: [],
+  modelChooseId: '',
+  modelChooseName: [],
+  onLoad: function (e) {
+    app.onPageLoad(e);
+  },
+  dataInitial: function () {
+    app.pageDataInitial();
+  },
+  onShareAppMessage: function (e) {
+    return app.onPageShareAppMessage(e);
+  },
+  onShow: function () {
+    app.onPageShow();
+  },
+  reachBottomFuc: [],
+  onReachBottom: function () {
+    app.onPageReachBottom( this.reachBottomFuc );
+  },
+  onUnload: function () {
+    app.onPageUnload();
+  },
+  tapPrevewPictureHandler: function (e) {
+    app.tapPrevewPictureHandler(e);
+  },
+  suspensionBottom: function () {
+    app.suspensionBottom();
+  },
+  pageScrollFunc: function (e) {
+    app.pageScrollFunc(e);
+  },
+  dynamicVesselScrollFunc: function (e) {
+    app.dynamicVesselScrollFunc(e);
+  },
+  goodsScrollFunc: function (e) {
+    app.goodsScrollFunc(e);
+  },
+  takeoutStyleScrollFunc: function(e){
+    app.takeoutStyleScrollFunc(e);
+  },
+  franchiseeScrollFunc: function (e) {
+    app.franchiseeScrollFunc(e);
+  },
+  seckillScrollFunc: function (e) {
+    app.seckillScrollFunc(e);
+  },
+  videoScrollFunc: function (e) {
+    app.videoScrollFunc(e);
+  },
+  carouselVideoClose: function(e) {
+    app.carouselVideoClose(e);
+  },
+  changeCount: function (e) {
+    app.changeCount(e);
+  },
+  inputChange: function (e) {
+    app.inputChange(e);
+  },
+  bindDateChange: function (e) {
+    app.bindDateChange(e);
+  },
+  bindTimeChange: function (e) {
+    app.bindTimeChange(e);
+  },
+  bindSelectChange: function (e) {
+    app.bindSelectChange(e);
+  },
+  bindScoreChange: function (e) {
+    app.bindScoreChange(e);
+  },
+  submitForm: function (e) {
+    app.submitForm(e);
+  },
+  udpateVideoSrc: function (e) {
+    app.udpateVideoSrc(e);
+  },
+  tapMapDetail: function (e) {
+    app.tapMapDetail(e);
+  },
+  uploadFormImg: function (e) {
+    app.uploadFormImg(e);
+  },
+  deleteUploadImg: function (e) {
+    app.deleteUploadImg(e);
+  },
+  listVesselTurnToPage: function (e) {
+    app.listVesselTurnToPage(e);
+  },
+  dynamicVesselTurnToPage: function (e) {
+    app.dynamicVesselTurnToPage(e);
+  },
+  userCenterTurnToPage: function (e) {
+    app.userCenterTurnToPage(e);
+  },
+  turnToGoodsDetail: function (e) {
+    app.turnToGoodsDetail(e);
+  },
+  turnToFranchiseeDetail: function (e) {
+    app.turnToFranchiseeDetail(e);
+  },
+  turnToSeckillDetail: function (e) {
+    app.turnToSeckillDetail(e);
+  },
+  sortListFunc: function (e) {
+    app.sortListFunc(e);
+  },
+  bbsInputComment: function (e) {
+    app.bbsInputComment(e);
+  },
+  bbsInputReply: function (e) {
+    app.bbsInputReply(e);
+  },
+  uploadBbsCommentImage: function (e) {
+    app.uploadBbsCommentImage(e);
+  },
+  uploadBbsReplyImage: function (e) {
+    app.uploadBbsReplyImage(e);
+  },
+  deleteCommentImage: function (e) {
+    app.deleteCommentImage(e);
+  },
+  deleteReplyImage: function (e) {
+    app.deleteReplyImage(e);
+  },
+  bbsPublishComment: function (e) {
+    app.bbsPublishComment(e);
+  },
+  clickBbsReplyBtn: function (e) {
+    app.clickBbsReplyBtn(e);
+  },
+  bbsPublishReply: function (e) {
+    app.bbsPublishReply(e);
+  },
+  searchList: function (e) {
+    app.searchList(e);
+  },
+  selectLocal: function (e) {
+    app.selectLocal(e);
+  },
+  cancelCity: function (e) {
+    app.cancelCity(e);
+  },
+  bindCityChange: function (e) {
+    app.bindCityChange(e);
+  },
+  submitCity: function (e) {
+    app.submitCity(e);
+  },
+  openTakeoutLocation: function (e) {
+    app.openTakeoutLocation(e);
+  },
+  callTakeout: function (e) {
+    app.callTakeout(e);
+  },
+  getMoreAssess: function (e) {
+    app.getMoreAssess(e);
+  },
+  changeEvaluate: function (e) {
+    app.changeEvaluate(e)
+  },
+  deleteAllCarts: function (e) {
+    app.deleteAllCarts(e);
+  },
+  clickCategory: function (e) {
+    app.clickCategory(e);
+  },
+  goodsListMinus: function (e) {
+    app.goodsListMinus(e);
+  },
+  goodsListPlus: function (e) {
+    app.goodsListPlus(e);
+  },
+  cartListMinus: function (e) {
+    app.cartListMinus(e);
+  },
+  cartListPlus: function (e) {
+    app.cartListPlus(e);
+  },
+  changeAssessType: function (e) {
+    app.changeAssessType(e);
+  },
+  showShoppingCartPop: function (e) {
+    app.showShoppingCartPop(e);
+  },
+  hideShoppingCart: function (e) {
+    app.hideShoppingCart(e);
+  },
+  showGoodsDetail: function (e) {
+    app.showGoodsDetail(e);
+  },
+  hideDetailPop: function (e) {
+    app.hideDetailPop(e);
+  },
+  hideModelPop: function (e) {
+    app.hideModelPop(e);
+  },
+  chooseModel: function (e) {
+    app.chooseModel(e);
+  },
+  sureChooseModel: function (e) {
+    app.sureChooseModel(e);
+  },
+  clickChooseComplete: function (e) {
+    app.clickChooseComplete(e);
+  },
+  reLocalAddress: function(e){
+    app.reLocalAddress(e);
+  },
+  tapGoodsTradeHandler: function (e) {
+    app.tapGoodsTradeHandler(e);
+  },
+  tapVideoHandler: function(e){
+    app.tapVideoHandler(e);
+  },
+  tapVideoPlayHandler: function(e){
+    app.tapVideoPlayHandler(e);  
+  },
+  tapVideoHandler: function(e){
+    app.tapVideoHandler(e);
+  },
+  tapInnerLinkHandler: function (e) {
+    app.tapInnerLinkHandler(e);
+  },
+  tapPhoneCallHandler: function (e) {
+    app.tapPhoneCallHandler(e);
+  },
+  tapRefreshListHandler: function (e) {
+    app.tapRefreshListHandler(e);
+  },
+  tapGetCouponHandler: function (e) {
+    app.tapGetCouponHandler(e);
+  },
+  tapCommunityHandler: function (e) {
+    app.tapCommunityHandler(e);
+  },
+  tapPageShareHandler:function(e) {
+    app.tapPageShareHandler(e);
+  },
+  turnToCommunityPage: function (e) {
+    app.turnToCommunityPage(e);
+  },
+  tapToFranchiseeHandler: function (e) {
+    app.tapToFranchiseeHandler(e);
+  },
+  tapToTransferPageHandler: function () {
+    app.tapToTransferPageHandler();
+  },
+  tapToSeckillHandler: function (e) {
+    app.tapToSeckillHandler(e);
+  },
+  tapToPromotionHandler: function () {
+    app.tapToPromotionHandler();
+  },
+  tapToCouponReceiveListHandler: function () {
+    app.tapToCouponReceiveListHandler();
+  },
+  tapToRechargeHandler: function () {
+    app.tapToRechargeHandler();
+  },
+  tapToXcx: function (e) {
+    app.tapToXcx(e);
+  },
+  tapFranchiseeLocation: function (e) {
+    app.tapFranchiseeLocation(e);
+  },
+  showAddShoppingcart: function (e) {
+    app.showAddShoppingcart(e);
+  },
+  hideAddShoppingcart: function () {
+    app.hideAddShoppingcart();
+  },
+  selectGoodsSubModel: function (e) {
+    app.selectGoodsSubModel(e);
+  },
+  resetSelectCountPrice: function () {
+    app.resetSelectCountPrice();
+  },
+  // 电商
+  clickGoodsMinusButton: function (e) {
+    app.clickGoodsMinusButton();
+  },
+  clickGoodsPlusButton: function (e) {
+    app.clickGoodsPlusButton();
+  },
+  sureAddToShoppingCart: function () {
+    app.sureAddToShoppingCart();
+  },
+  sureAddToBuyNow: function () {
+    app.sureAddToBuyNow();
+  },
+  clickTostoreMinusButton: function (e) {
+    app.clickTostoreMinusButton(e);
+  },
+  clickTostorePlusButton: function (e) {
+    app.clickTostorePlusButton(e);
+  },
+  readyToPay: function () {
+    app.readyToTostorePay();
+  },
+  getValidateTostore: function () {
+    app.getValidateTostore();
+  },
+  goToShoppingCart: function () {
+    app.goToShoppingCart();
+  },
+  getCartList: function () {
+    app.getTostoreCartList();
+  },
+  stopPropagation: function () {
+  },
+  turnToSearchPage:function (e) {
+    app.turnToSearchPage(e);
+  },
+  previewImage: function (e) {
+    var dataset = e.currentTarget.dataset;
+    app.previewImage({
+      current : dataset.src,
+      urls: dataset.previewImgarr,
+    });
+  },
+  scrollPageTop: function () {
+    app.pageScrollTo(0);
+  },
+  suspensionTurnToPage: function (e) {
+    app.suspensionTurnToPage(e);
+  },
+   tapToLuckyWheel: function (e) {
+    app.tapToLuckyWheel(e);
+  },
+  tapToGoldenEggs: function (e) {
+    app.tapToGoldenEggs(e);
+  },
+  tapToScratchCard: function (e) {
+    app.tapToScratchCard(e);
+  },
+  tapToLuckyWheel: function (e) {
+    app.tapToLuckyWheel(e);
+  },
+  keywordList:{},
+  bindSearchTextChange: function (e) {
+    this.keywordList[e.currentTarget.dataset.compid] = e.detail.value;
+  },
+  // 文字组件跳到地图
+  textToMap: function(e) {
+    app.textToMap(e);
+  },
+  tapDynamicClassifyFunc: function(e){
+    app.tapDynamicClassifyFunc(e);
+  },
+  // 跳转到视频详情
+  turnToVideoDetail : function(e) {
+    app.turnToVideoDetail(e);
+  },
+  // 单个视频组件播放视频
+  startPlayVideo : function(e) {
+    app.startPlayVideo(e);
+  },
+  // 视频播放报错
+  videoError: function(e) {
+    app.showModal({
+      content: e.detail.errMsg
+    });
+  }
+};
+Page(pageData);
